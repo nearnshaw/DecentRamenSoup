f286febd8b2debffc3ae732b405476732018a0e6
nearnshaw <nearnshaw@decentraland.org>
2019-01-25 12:34:11 -0300

    drop en el grid

--- src/game.ts
+++ src/game.ts
@@ -44,21 +44,23 @@ objectGrabber.add(new ObjectGrabberComponent());
 engine.addEntity(objectGrabber);
 
 export class ObjectGrabberSystem implements ISystem {
-  transform = objectGrabber.get(Transform);
-  objectGrabberComponent = objectGrabber.get(ObjectGrabberComponent);
+  transform = objectGrabber.get(Transform)
+  objectGrabberComponent = objectGrabber.get(ObjectGrabberComponent)
 
   constructor() {
     input.subscribe("BUTTON_A_DOWN", e => {
        for (let thing of grabableStuff.entities) {
-        let picked = thing.get(GrabableObjectComponent);
+        let picked = thing.get(GrabableObjectComponent)
         if (picked.grabbed) {
-          picked.grabbed = false;
-          thing.setParent(null);
-          this.objectGrabberComponent.grabbedObject = null;
+          picked.grabbed = false
+          let pos = getClosestPos()
+          thing.setParent(pos)
+          thing.get(Transform).position = Vector3.Zero()
+          this.objectGrabberComponent.grabbedObject = null
 
-          let newPos = camera.position.add(Vector3.Forward());
-          newPos.y = 0.5;
-          thing.get(Transform).position = newPos;
+          //let newPos = camera.position.add(Vector3.Forward())
+          //newPos.y = 0.5
+          
         }
       } 
     });
@@ -129,7 +131,6 @@ box2.add(
 engine.addEntity(box2);
 
 // create grid
-
 let shelves: number[][] = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 1, 1, 1, 1, 1, 0],
@@ -164,4 +165,33 @@ for (let x = 0; x < xMax; x ++){
     }))
     engine.addEntity(testEnt)
   }
+}
+
+// get closest when dropping
+function getClosestPos(){
+  let closestDist = 100
+  let closestGridPos = null
+  for (let place of gridPositions.entities) {
+    let pos = place.get(Transform).position
+    let d = distance(camera.position, pos)
+    if (d < closestDist){
+      closestDist = d
+      closestGridPos = place
+    }
+  }
+  return closestGridPos  
+}
+
+
+// Get distance
+/* 
+Note:
+This function really returns distance squared, as it's a lot more efficient to calculate.
+The square root operation is expensive and isn't really necessary if we compare the result to squared values.
+We also use {x,z} not {x,y}. The y-coordinate is how high up it is.
+*/
+function distance(pos1: Vector3, pos2: Vector3): number {
+  const a = pos1.x - pos2.x
+  const b = pos1.z - pos2.z
+  return a * a + b * b
 }
